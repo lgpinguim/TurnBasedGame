d0a32566b946cf17428f0bce97f1dd235cd36418
Luis Ferreira <lg_pinguim@hotmail.com>
2024-02-13 14:05:41 +0000

    backup

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField] private Animator unitAnimator;
     private Vector3 targetPosition;
 
     private void Update()
     {
+       
         const float stoppingDistance = .1f;
         if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
         {
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             float moveSpeed = 4f;
             transform.position += moveDirection * (moveSpeed * Time.deltaTime);
+            unitAnimator.SetBool("IsWalking",true);
+        }
+        else
+        {
+            unitAnimator.SetBool("IsWalking",false);
         }
 
         if (Input.GetMouseButtonDown(0))
